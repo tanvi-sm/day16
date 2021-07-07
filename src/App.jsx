2f2f341f677022126b9929d697a5e761a91f0522
tanvi-sm <76934108+tanvi-sm@users.noreply.github.com>
2021-07-07 21:29:05 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./App.css";
+import Card from './Card';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="heading">MEME CARD</h1>
+        <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Card.css"
+
+function Card() {
+    return (
+    <div className="card_container">
+      
+      <div className="image"><img src="https://devhumor.com/content/uploads/images/February2021/interview_prep.jpg" alt="logo" /></div>
+      <h1>Recruiting meme</h1>
+    </div>
+    )
+}
+
+export default Card;
